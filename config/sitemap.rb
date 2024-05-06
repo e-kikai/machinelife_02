15c5b236e72342fc722f21192ac6c9cce05bc72c
ba-ta <bata44883@gmail.com>
2024-05-06 13:19:41 +0900

    サイトマップ修正

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -6,11 +6,11 @@ SitemapGenerator::Sitemap.create do
 
   ### 商品詳細 ###
   Machine.sales.only_machines.pluck(:id).each do |id|
-    add "/products/#{id}", priority: 0.95, changefreq: 'daily'
+    add "/machines/#{id}", priority: 0.95, changefreq: 'daily'
   end
 
   Machine.sales.only_tools.pluck(:id).each do |id|
-    add "/products/#{id}", priority: 0.9, changefreq: 'daily'
+    add "/machines/#{id}", priority: 0.9, changefreq: 'daily'
   end
 
   ### 新着商品 ###
@@ -19,26 +19,26 @@ SitemapGenerator::Sitemap.create do
 
   ### 大ジャンル ###
   LargeGenre.where(xl_genre_id: XlGenre::MACHINE_IDS).pluck(:id).each do |id|
-    add "/products/large_genre/#{id}", priority: 0.75, changefreq: 'daily'
+    add "/machines/large_genre/#{id}", priority: 0.75, changefreq: 'daily'
   end
 
   LargeGenre.where.not(xl_genre_id: XlGenre::MACHINE_IDS).pluck(:id).each do |id|
-    add "/products/large_genre/#{id}", priority: 0.7, changefreq: 'daily'
+    add "/machines/large_genre/#{id}", priority: 0.7, changefreq: 'daily'
   end
 
   ### ジャンル ###
   Genre.pluck(:id).each do |id|
-    add "/products/genre/#{id}", priority: 0.65, changefreq: 'daily'
+    add "/machines/genre/#{id}", priority: 0.65, changefreq: 'daily'
   end
 
   ### メーカー ###
   Machine.sales.where.not(maker2: "").distinct.pluck(Arel.sql("COALESCE(makers.maker_master, machines.maker2) as ma")).each do |maker|
-    add "/products/maker/#{maker}", priority: 0.6, changefreq: 'daily'
+    add "/machines/maker/#{maker}", priority: 0.6, changefreq: 'daily'
   end
 
   ### 出品会社 ###
   Machine.sales.distinct.pluck(:company_id).each do |id|
     add "/companies/#{id}", priority: 0.65, changefreq: 'daily'
-    add "/products/company/#{id}", priority: 0.65, changefreq: 'daily'
+    add "/machines/company/#{id}", priority: 0.65, changefreq: 'daily'
   end
 end
