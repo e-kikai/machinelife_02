469ceab364dd6c8e13f2eea66a195b1ff507c64e
ba-ta <bata44883@gmail.com>
2024-05-07 15:58:55 +0900

    協賛企業

--- crawlers/crawler/kobayashi.rb
+++ crawlers/crawler/kobayashi.rb
@@ -144,6 +144,7 @@ class Kobayashi < Base
       @log.debug(temp)
     rescue StandardError => e
       error_report("", e)
+      raise
     end
 
     self
@@ -155,11 +156,11 @@ class Kobayashi < Base
 
     ### 並列クロール ###
     locks = Queue.new
-    1.times { locks.push :lock }
+    5.times { locks.push :lock }
 
     Array.new(m[1].to_i.fdiv(60).ceil) do |i|
       uri = "https://www.kkmt.co.jp/products?display_mode=table&page=#{i + 1}&pictures=no_own"
-      sleep 0.1
+      sleep 0.3
 
       Thread.new do
         lock = locks.pop
@@ -169,7 +170,7 @@ class Kobayashi < Base
           try += 1
           @log.info("-> #{uri}")
           scrape(@a.get(uri))
-        rescue StandardError => e
+        rescue StandardError
           @log.info("retry:#{try} -> #{uri}")
           retry if try <= 3
         end
