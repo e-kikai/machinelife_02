a1b8dc27d75952230327940a50d44d6645d4d68c
ba-ta <bata44883@gmail.com>
2024-05-11 20:10:11 +0900

    在庫詳細アクセスログ_05

--- app/controllers/system/detail_logs_controller.rb
+++ app/controllers/system/detail_logs_controller.rb
@@ -4,6 +4,7 @@ class System::DetailLogsController < System::ApplicationController
     @detail_logs = DetailLog.includes({ machine: [:company] }, :user, :company).order(id: :desc)
       .then { |ds| params[:start_date].present? ? ds.where(created_at: params[:start_date]..) : ds }
       .then { |ds| params[:end_date].present?   ? ds.where(created_at: ..params[:end_date]) : ds }
+      .then { |ds| params[:user_id].present?    ? ds.where(user_id: params[:user_id]) : ds }
       .then { |ds| params[:k].present?          ? ds.where_keyword(params[:k]) : ds }
 
     ### ページャ ###
