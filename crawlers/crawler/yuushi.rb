e9f908de648294e98b91c4d86d80223b9a675d13
ba-ta <bata44883@gmail.com>
2024-05-17 02:06:07 +0900

    ユウシクローラ

--- crawlers/crawler/yuushi.rb
+++ crawlers/crawler/yuushi.rb
@@ -20,9 +20,7 @@ class Yuushi < Base
 
     @company    = '株式会社ユウシ'
     @company_id = 25
-    # @start_uri  = 'https://www.chuuko-kikai.com/item-list.php'
-
-    @start_uri = "http://www.chuuko-kikai.com/item-list.php?blog_categ1=1"
+    @start_uri = "https://www.chuuko-kikai.com/item-list.php"
     @crawl_allow = /xxxxx/
     @crawl_deny  = //
   end
@@ -33,40 +31,51 @@ class Yuushi < Base
   #
   def scrape
     #### ページ情報のスクレイピング ####
-    @log.debug @p
-    @p.search('a[href*="blog_id1"]').each do |m|
-      @log.debug(m)
+    @p = nokogiri(@start_uri)
+
+    @p.search('.wb-blog-list').each do |m|
+      uid = m.at('.number').text.f.delete("管理番号")
+
+      next unless check_uid(uid)
+
       #### ディープクロール ####
-      detail_uri = join_uri(@p.uri, m[:href])
+      link = m.at('a[href*=blog_id1]')
+      detail_uri = join_uri(@start_uri, link[:href])
+
       begin
         p2 = nokogiri(detail_uri)
-
-        uid = p2.at(".item-detail-table table:nth(1) td:nth(1)").text.f
-        next unless check_uid(uid)
 
         temp = {
           uid:,
           no: uid,
-          name: p2.at(".item-detail-table table:nth(1) tr:nth(2) td").text.f,
-          maker: p2.at(".item-detail-table table:nth(1) tr:nth(3) td").text.f,
-          model: p2.at(".item-detail-table table:nth(1) tr:nth(4) td").text.f,
-          year: p2.at(".item-detail-table table:nth(1) tr:nth(5) td").text.f,
-          spec: p2.at(".item-detail-table table:nth(2) tr:nth(1) td").text.f,
-          accessory: p2.at(".item-detail-table table:nth(2) tr:nth(2) td").text.f,
-          comment: p2.at(".item-detail-table table:nth(2) tr:nth(3) td").text.f,
-          location: "本社",
-          hint: p2.at(".item-detail-table table:nth(1) tr:nth(2) td").text.f
+          name: "",
+          maker: "",
+          location: "本社"
         }
+
+        p2.search(".item-detail-table tr").each do |tr|
+          td = tr.at("td").text.f
+          case tr.at("th").text.f
+          when "機械名"; temp[:name] = td
+          when "メーカー"; temp[:maker] = td
+          when "型式"; temp[:model] = td
+          when "年式"; temp[:year] = td
+          when "仕様"; temp[:spec] = td
+          when "付属品"; temp[:accessory] = td
+          when "コメント"; temp[:comment] = td
+          end
+
+          temp[:hint] = temp[:name].gsub(/(（.*)$/, "")
+        end
 
         next if temp[:maker].include?("●")
 
         # 画像
-        temp[:used_imgs] = []
-        p2.search('.img-sub img').each do |i|
+        temp[:used_imgs] = p2.search('.img-sub img').filter_map do |i|
           next unless i[:src].match?(/(jpg|JPG|jpeg|JPEG)$/)
           next if i[:src].include?("image_noimage")
 
-          temp[:used_imgs] << join_uri(detail_uri, i[:src])
+          join_uri(detail_uri, i[:src])
         end
 
         @d << temp if temp[:name]
