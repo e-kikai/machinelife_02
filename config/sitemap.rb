7043dc841f746dfc923cc3bb617669b86f405480
ba-ta <bata44883@gmail.com>
2024-06-05 18:34:51 +0900

    サイトマップ

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,6 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://www.zenkiren.net"
+SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 
 SitemapGenerator::Sitemap.create do
   add '/', priority: 1.0, changefreq: 'daily'
