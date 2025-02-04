dae31966a25031b51b03cdd4e608b52fed71ef4a
ba-ta <bata44883@gmail.com>
2025-02-04 12:43:45 +0900

    MAI在庫検索ログ_01

--- app/controllers/system/mai_search_logs_controller.rb
+++ app/controllers/system/mai_search_logs_controller.rb
@@ -18,30 +18,36 @@ class System::MaiSearchLogsController <System::ApplicationController
       @rows = (Date.new(2024, 11, 1)..today).select { |date| date.day == 1 }.map { |d| d.strftime('%Y/%m') }.reverse
 
       @mai_search_logs = MaiSearchLog.ignore_hosts.group("to_char(created_at, 'YYYY/MM')")
-      @detail_logs     = DetailLog.ignore_hosts.group("to_char(created_at, 'YYYY/MM')").where("r LIKE '%mai%'")
-      @contacts        = Contact.group("to_char(created_at, 'YYYY/MM')").where("r LIKE '%mai%'")
+      @detail_logs_all = DetailLog.ignore_hosts.group("to_char(created_at, 'YYYY/MM')")
+      @detail_logs     = @detail_logs_all.where("r LIKE '%mai%'")
+      @contacts_all    = Contact.where.not(machine_id: nil).where.not(company_id: nil).group("to_char(created_at, 'YYYY/MM')")
+      @contacts        = @contacts_all.where("r LIKE '%mai%'")
     else
       @month = params[:month] ? params[:month].to_date : today
 
       @mai_search_logs = MaiSearchLog.ignore_hosts.group("DATE(mai_search_logs.created_at)").where(created_at: @month.in_time_zone.all_month)
-      @contacts        = Contact.group("DATE(contacts.created_at)").where(created_at: @month.in_time_zone.all_month).where("r LIKE '%mai%'")
-      @detail_logs     = DetailLog.ignore_hosts.group("DATE(detail_logs.created_at)").where(created_at: @month.in_time_zone.all_month).where("r LIKE '%mai%'")
+      @detail_logs_all = DetailLog.ignore_hosts.group("DATE(detail_logs.created_at)").where(created_at: @month.in_time_zone.all_month)
+      @detail_logs     = @detail_logs_all.where("r LIKE '%mai%'")
+      @contacts_all    = Contact.where.not(machine_id: nil).where.not(company_id: nil).group("DATE(contacts.created_at)").where(created_at: @month.in_time_zone.all_month)
+      @contacts        = @contacts_all.where("r LIKE '%mai%'")
 
       @rows = @month.all_month.to_a
     end
 
-    @mai_search_logs_count = @mai_search_logs.count
+    @mai_search_logs_count     = @mai_search_logs.count
     @mai_search_logs_utag_count = @mai_search_logs.distinct.count(:utag)
     @mai_search_logs_good_count = @mai_search_logs.where(good: true).count
     @mai_search_logs_bad_count  = @mai_search_logs.where(bad: true).count
 
     @contacts_sum_count     = @contacts.count
+    @contacts_all_count     = @contacts_all.count
     @contacts_utag_count    = @contacts.distinct.count(:utag)
     @contacts_adv_count     = @contacts.where("r LIKE '%adv%'").count
     @contacts_blk_count     = @contacts.where("r LIKE '%blk%'").count
     @contacts_crd_count     = @contacts.where("r LIKE '%crd%'").count
 
     @detail_logs_sum_count  = @detail_logs.count
+    @detail_logs_all_count  = @detail_logs_all.count
     @detail_logs_utag_count = @detail_logs.distinct.count(:utag)
     @detail_logs_adv_count  = @detail_logs.where("r LIKE '%adv%'").count
     @detail_logs_crd_count  = @detail_logs.where("r LIKE '%crd%'").count
