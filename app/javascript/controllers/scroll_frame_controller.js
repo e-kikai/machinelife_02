55485731bb6a5a44f986b552e77281cdf8ffc838
ba-ta <bata44883@gmail.com>
2025-06-11 08:49:38 +0900

    トップページアイコン_01

--- app/javascript/controllers/scroll_frame_controller.js
+++ app/javascript/controllers/scroll_frame_controller.js
@@ -1,45 +1,25 @@
-// // controllers/scroll_frame_controller.js
-// import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus"
 
-// export default class extends Controller {
-//   static targets = ["topButton"]
+export default class extends Controller {
+  static targets = ["button"]
 
-//   connect() {
-//     // Turbo Frameが更新されたら上へスクロール
-//     this.frameLoadHandler = (event) => {
-//       window.scrollTo({ top: 0, behavior: "smooth" });
-//     }
-//     document.addEventListener("turbo:frame-load", this.frameLoadHandler);
+  connect() {
+    this.toggle = this.toggle.bind(this)
+    window.addEventListener("scroll", this.toggle)
+    this.toggle() // 初期状態の判定
+  }
 
-//     // スクロールイベントでボタン出し分け
-//     this.toggleButton = this.toggleButton.bind(this);
-//     window.addEventListener("scroll", this.toggleButton);
+  disconnect() {
+    window.removeEventListener("scroll", this.toggle)
+  }
 
-//     this.toggleButton();
-//   }
+  toggle() {
+    const show = window.scrollY > 200
+    this.buttonTarget.classList.toggle("d-none", !show)
+  }
 
-//   disconnect() {
-//     document.removeEventListener("turbo:frame-load", this.frameLoadHandler);
-//     window.removeEventListener("scroll", this.toggleButton);
-//   }
-
-//   toggleButton() {
-//     console.log("bbb")
-
-//     if (!this.hasTopButtonTarget) return;
-//     // 200px以上スクロールしたら▲ボタン表示
-//     if (window.scrollY > 200) {
-//       this.topButtonTarget.classList.remove("d-none");
-//     } else {
-//       this.topButtonTarget.classList.add("d-none");
-//     }
-
-
-
-//   }
-
-//   scrollTop(event) {
-//     event.preventDefault();
-//     window.scrollTo({ top: 0, behavior: "smooth" });
-//   }
-// }
+  scroll() {
+    event.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" })
+  }
+}
